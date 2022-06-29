0edbf4c350891c4a688856d6ad87840c8b297cb8
Pukkysasikan <sasikan.in.62@ubu.ac.th>
2022-06-29 14:35:31 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Prototype-tax-invoice',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
